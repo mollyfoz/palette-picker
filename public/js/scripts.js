a79d8225b0c7b5c3260a1b67e7f07e7211e79253
Molly Foster <mollyrfoster@gmail.com>
2017-10-03 17:38:14 -0600

    randomize and lock colors

---
+++ public/js/scripts.js
@@ -0,0 +1,28 @@
+const generateNewColors = () => {
+  $('.color-card').each((i, color) => {
+      const randomColor = "#000000".replace(/0/g,function(){
+        return (~~(Math.random()*16)).toString(16) })
+      !$(color).hasClass('locked') ? $(color).css('background-color', randomColor) : null
+      $(color).text(randomColor.toUpperCase())
+    })
+
+}
+
+const toggleLock = (e) => $(e.target).toggleClass('locked')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(generateNewColors)
+$('.generate-new-btn').click( () => generateNewColors())
+$('.color-card').click((e) => toggleLock(e))
